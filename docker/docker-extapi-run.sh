f78ece4d52db1277b710d5bd58b6a1547569b5ab
gsokolov <gsokolov@anotheria.net>
2022-04-29 12:25:38 +0300

    docker scripts

---
+++ docker/docker-extapi-run.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+docker run \
+    --rm \
+    --network chagelog-network \
+    --name changelog-extapi \
+    -p 8080:8080 \
+    -e CONFIGUREME_ENVIRONMENT=dev_gsokolov_docker \
+    -d \
+    changelog
