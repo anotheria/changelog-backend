36965df76095cddfd283aad12544f23e8b3812ae
gsokolov <gsokolov@anotheria.net>
2022-04-29 14:09:40 +0300

    docker scripts

--- docker/docker-postgres-run.sh
+++ docker/docker-postgres-run.sh
@@ -1,13 +1,13 @@
 #!/usr/bin/env bash
 
 docker run \
-    --rm \
+    `#--rm` \
     --network chagelog-network \
     --name changelog-postgres \
     -p 6432:5432 \
     -e POSTGRES_USER=changelog \
     -e POSTGRES_PASSWORD=changelog \
     -e POSTGRES_DB=changelog \
-    -e PGDATA=/Users/ano/IdeaProjects/changelog/changelog-postgres/data/pgdata \
+    `#-e PGDATA=/Users/ano/IdeaProjects/changelog/changelog-postgres/data/pgdata` \
     -d \
     postgres:13.3
